b8c909aa861db55f23a865ae3cdf182154b365b1
JMarques12 <joaovcmarques12@gmail.com>
2024-03-08 10:43:15 -0300

    att

---
+++ Projeto/api/src/controllers/rota.js
@@ -0,0 +1,73 @@
+const con = require('../connection/mysql');
+
+//CRUD - CREATE
+const addrota = (req, res) => {
+    if(req.body!=null && req.body.idrota!=null && req.body.origem!=null &&  req.body.pedido!=null!=null &&  req.body.distancia){
+        const { idrota, origem, pedido, distancia } = req.body;
+        con.query('INSERT INTO rotas (idrota, origem, pedido, distancia) VALUES (?, ?,?,?)', [idrota, origem, pedido, distancia], (err, result) => {
+            if (err) {
+                res.status(500).send('Erro ao adicionar rota');
+            }
+            res.status(201).send('rota adicionada com sucesso');
+        });
+    } else {
+        res.status(500).send('Favor enviar todos os campos obrigatórios');
+    }
+}
+//CRUD - READ
+const getrota = (req, res) => {
+    if (req.params.origem != null) {
+        con.query(`SELECT * FROM rotas WHERE func = '${req.params.origem}'`, (err, result) => {
+            if (err) {
+                res.status(500).send('Erro ao listar rota');
+            }
+            res.json(result);
+        });
+    } else {
+        con.query('SELECT * FROM rotas', (err, result) => {
+            if (err) {
+                res.status(500).send('Erro ao listar rota');
+            }
+            res.json(result);
+        });
+    }
+}
+
+//CRUD - UPDATE
+const updaterota = (req, res) => {
+    if(req.body!=null && req.body.idrota!=null && req.body.origem!=null &&  req.body.pedido!=null!=null &&  req.body.distancia){
+        const { idrota, origem, pedido, distancia } = req.body;
+        con.query('UPDATE pedidos SET idrota = ?, origem = ?, pedido = ?,  WHERE distancia = ?', [idrota, origem, pedido, distancia], (err, result) => {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                res.status(200).json(req.body);
+            }
+        });
+    } else {
+        res.status(400).json('Favor enviar todos os campos obrigatórios');
+    }
+}
+
+//CRUD - DELETE
+const deleterota = (req, res) => {
+    if (req.params.origem!= null) {
+        con.query(`DELETE FROM rotas WHERE func = '${req.params.origem}'`, (err, result) => {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                if (result.affectedRows == 0) {
+                    res.status(404).json('rota não encontrada');
+                } else {
+                    res.status(200).json('rota deletada com sucesso');
+                }
+            }
+        });
+    } 
+}
+module.exports = {
+    addrota,
+    getrota,
+    updaterota,
+    deleterota
+}
